4d4a89c356aa969a6b8b5edb3d3cd75f1eaf132c
ayoub <ayoubzoubeidi@gmail.com>
2022-12-06 21:20:03 +0100

    added Operation and currency persistence classes.

---
+++ conversion-operation/src/main/java/tn/zoubeidi/conversionoperation/domain/Operation.java
@@ -0,0 +1,29 @@
+package tn.zoubeidi.conversionoperation.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Operation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Currency currencyFrom;
+    private Currency currencyTo;
+
+    private BigDecimal initialAmount;
+    private BigDecimal conversionRate;
+}
