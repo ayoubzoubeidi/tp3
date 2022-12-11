d189658cf23278d6f9850f0f608ea048085bb9eb
ayoub <ayoubzoubeidi@gmail.com>
2022-12-11 10:54:00 +0100

    update conversion rate address

--- conversion-operation/src/main/java/tn/zoubeidi/conversionoperation/service/rest/RestUtil.java
+++ conversion-operation/src/main/java/tn/zoubeidi/conversionoperation/service/rest/RestUtil.java
@@ -3,6 +3,6 @@ package tn.zoubeidi.conversionoperation.service.rest;
 public class RestUtil {
 
     public static String CONVERSION_RATE_MAIN_API = "http//localhost:8081";
-    public static String CONVERSION_API = CONVERSION_RATE_MAIN_API + "/convert/{from}/{to}";
+    public static String CONVERSION_API = CONVERSION_RATE_MAIN_API + "/api/v1/conversion/{from}/{to}/now";
 
 }
